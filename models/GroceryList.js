b1e8ff1c033ed2b1b2ef742211f651fcac4684ba
Andrew Connally <ajconnally93@gmail.com>
2026-04-19 16:09:06 -0500

    defined User schema and GroceryList schema

---
+++ models/GroceryList.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+
+const groceryListItemSchema = new mongoose.Schema(
+  {
+    ingredientId: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    totalQuantity: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+    unit: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    estimatedLineCost: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+    sourceMealIds: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Meal'
+        }
+      ],
+      default: []
+    }
+  },
+  { _id: false }
+);
+
+const groceryListSchema = new mongoose.Schema(
+  {
+    mealPlanId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'MealPlan',
+      required: true
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    items: {
+      type: [groceryListItemSchema],
+      default: []
+    },
+    estimatedTotalCost: {
+      type: Number,
+      required: true,
+      default: 0,
+      min: 0
+    },
+    generatedAt: {
+      type: Date,
+      required: true,
+      default: Date.now
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+);
+
+module.exports = mongoose.model('GroceryList', groceryListSchema);
--- models/MealPlan.js
+++ models/MealPlan.js
@@ -20,6 +20,7 @@ const mealPlanSchema = new mongoose.Schema(
   {
     userId: {
       type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
       required: true
     },
     status: {
